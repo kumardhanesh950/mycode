bbf2c54fa8343c523867396d9ae7ccf1d3baad5a
kumardhanesh950 <65038899+kumardhanesh950@users.noreply.github.com>
2024-05-16 17:54:37 +0000

    conversion of Data types

---
+++ 01_basics/conversion.js
@@ -0,0 +1,27 @@
+let score = 'hitesh'
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInnumber= Number(score)
+// console.log(valueInnumber);
+// console.log(typeof valueInnumber)
+
+// "33" => 33   string ko jab hum number me convert kerte hen to wo convert hojata he
+// "33abc" => NaN jab hum 33abc ko cnvert kerte hen to wo number me covert hojata he but real me wo NaN rehta he
+// true = 1,  false =0 jab hum number ko boolean me convert kerte hen to 
+
+
+let isloggedIn =" "
+let NumberisloggedIn =Boolean(isloggedIn)
+console.log(NumberisloggedIn)
+
+//1 => true , 0 =>false
+// ""=> false, "hitesh" =>true
+
+
+//How to convert number in string
+let Somenumber= 33
+let SomenumberInString =String(Somenumber)
+console.log(SomenumberInString)
+console.log(typeof SomenumberInString)
+
